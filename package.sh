bc71a9ffc0b7865e814d36d7b46a07d068721e15
Exter-N <exter-n@exter-n.fr>
2024-03-24 07:26:31 +0100

    Generate version numbers from Git

--- package.sh
+++ package.sh
@@ -4,8 +4,9 @@ set -e
 
 cd "$(dirname "$0")"
 
-OUROBOROS_NAME="$(jq --raw-output '.Name + " v" + .Version' < penumbra/meta.json)"
-MDK_NAME="$(jq --raw-output '.Name + " v" + .Version' < penumbra_mdk/meta.json)"
+VERSION="$(git describe --tags --always | sed -e 's/^v\|^.*\?-v//')"
+OUROBOROS_NAME="$(jq --raw-output '.Name' < penumbra/meta.json) v${VERSION}"
+MDK_NAME="$(jq --raw-output '.Name' < penumbra_mdk/meta.json) v${VERSION}"
 
 rm -rf build/ouroboros
 rm -f "build/${OUROBOROS_NAME}.pmp"
@@ -20,6 +21,9 @@ INSTALL_DIR="build/ouroboros/shader" DK_INSTALL_DIR="build/mdk/shpk_devkit" ./bu
 
 cp -v penumbra/*.json build/ouroboros
 cp -v penumbra_mdk/*.json build/mdk
+
+jq --arg VERSION "${VERSION}" '.Version |= $VERSION' penumbra/meta.json > build/ouroboros/meta.json
+jq --arg VERSION "${VERSION}" '.Version |= $VERSION' penumbra_mdk/meta.json > build/mdk/meta.json
 
 cd build/ouroboros
 zip -r -9 "../${OUROBOROS_NAME}.pmp" .
